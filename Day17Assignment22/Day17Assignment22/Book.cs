fe4d402c81a7823ac78c60e1bf44310ac5ef3c57
onkar <jadhavonkar333@gmail.com>
2023-11-28 16:03:08 +0530

    assignment added

---
+++ Day17Assignment22/Day17Assignment22/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day17Assignment22
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public bool IsAvailable { get; set; }
+
+        // Constructor
+        public Book(int bookId, string title, string author, string genre, bool isAvailable)
+        {
+            BookId = bookId;
+            Title = title;
+            Author = author;
+            Genre = genre;
+            IsAvailable = isAvailable;
+        }
+
+    }
+}
---
+++ Day17Assignment22/Day17Assignment22/Library.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day17Assignment22
+{
+    public class Library
+    {
+        private List<Book> books;
+
+        public Library()
+        {
+            books = new List<Book>();
+        }
+
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+            Console.WriteLine("Book added successfully!");
+        }
+
+        public void ViewAllBooks()
+        {
+            Console.WriteLine("All Books in the Library:");
+
+            foreach (var book in books)
+            {
+                Console.WriteLine($"ID: {book.BookId}, Title: {book.Title}, Author: {book.Author}, Genre: {book.Genre}, Available: {book.IsAvailable}");
+            }
+        }
+
+        public void SearchBookById(int bookId)
+        {
+            Book foundBook = books.Find(book => book.BookId == bookId);
+
+            if (foundBook != null)
+            {
+                DisplayBookDetails(foundBook);
+            }
+            else
+            {
+                Console.WriteLine("Book not found.");
+            }
+        }
+
+
+        public void SearchBookByTitle(string title)
+        {
+            Book foundBook = books.Find(book => book.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+
+            if (foundBook != null)
+            {
+                DisplayBookDetails(foundBook);
+            }
+            else
+            {
+                Console.WriteLine("Book not found.");
+            }
+        }
+
+        private void DisplayBookDetails(Book book)
+        {
+            Console.WriteLine($"ID: {book.BookId}, Title: {book.Title}, Author: {book.Author}, Genre: {book.Genre}, Available: {book.IsAvailable}");
+        }
+    }
+}
